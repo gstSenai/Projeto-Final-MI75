2467836f257eca6177a28bd2924ac1bb32033688
GUSTAVO DE SOUZA <gustavo_souza12@estudante.sesisenai.org.br>
2025-02-20 16:48:20 -0300

    Adicionado o Backend da Aplicacao

---
+++ back-end-imobiliaria/Imobiliaria/src/test/java/weg/projetofinal/Imobiliaria/HavApplicationTests.java
@@ -0,0 +1,13 @@
+package weg.projetofinal.Imobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
