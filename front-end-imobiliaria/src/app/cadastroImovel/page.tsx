557ebc933223258b530ff9536cb80bc6c4d0961a
Caio dos Santos Moura <caiosantosmoura9@gmail.com>
2025-02-21 14:06:41 -0300

    update
    ajustes na pagina do Cadastro do Imovel

--- front-end-imobiliaria/src/app/cadastroImovel/page.tsx
+++ front-end-imobiliaria/src/app/cadastroImovel/page.tsx
@@ -37,6 +37,32 @@ export default function CadastroImovel() {
 
                 <hr className="mt-16 mb-4 w-full h-2 rounded-2xl bg-[#702632] max-lg:h-3 max-lg:mt-10"></hr>
 
+                <div className={`${inter.className} flex justify-between max-lg:justify-center `}>
+                    <div className="flex flex-row items-center max-lg:justify-center">
+                        <p className="text-2xl xl:text-4xl font-semibold my-10 max-lg:hidden">Endereço:</p>
+                    </div>
+                </div>
+
+                <div className="flex 2xl:gap-16">
+                    <form action="text" className="flex items-center max-lg:justify-center gap-6 lg:pr-24 xl:pr-28 xl:py-4 2xl:w-[700px] 2xl:py-6 pl-4 py-3.5
+                             bg-white border border-black rounded-2xl max-lg:bg-transparent max-lg:border-transparent 
+                             max-lg:p-0">
+                        <img src="/imagensForms/canetaEditar.png" alt="" />
+                        <input type="text" placeholder="Rua:" name="rua" className="text-[#5C5C5C]/80 max-sm:text-lg max-md:text-2xl max-lg:text-3xl xl:text-xl 2xl:text-xl whitespace-nowrap max-lg:text-black" />
+                    </form>
+                    <form action="text" className="flex items-center max-lg:justify-center gap-6 lg:pr-24 xl:pr-28 xl:py-4 2xl:w-[700px] 2xl:py-6 pl-4 py-3.5
+                             bg-white border border-black rounded-2xl max-lg:bg-transparent max-lg:border-transparent 
+                             max-lg:p-0">
+                        <img src="/imagensForms/canetaEditar.png" alt="" />
+                        <input type="text" placeholder="Cep:" name="cep" className="text-[#5C5C5C]/80 max-sm:text-lg max-md:text-2xl max-lg:text-3xl xl:text-xl 2xl:text-xl whitespace-nowrap max-lg:text-black" />
+                    </form>
+                    <form action="text" className="flex max-lg:justify-center gap-6 lg:pr-24 xl:pr-28 xl:py-4 2xl:w-[700px] 2xl:py-6 pl-4 py-3.5
+                             bg-white border border-black rounded-2xl max-lg:bg-transparent max-lg:border-transparent 
+                             max-lg:p-0">
+                        <img src="/imagensForms/canetaEditar.png" alt="" />
+                        <input type="text" placeholder="Número:" name="numero" className="text-[#5C5C5C]/80 max-sm:text-lg max-md:text-2xl max-lg:text-3xl xl:text-xl 2xl:text-xl whitespace-nowrap max-lg:text-black" />
+                    </form>
+                </div>
             </div>
         </>
     );
