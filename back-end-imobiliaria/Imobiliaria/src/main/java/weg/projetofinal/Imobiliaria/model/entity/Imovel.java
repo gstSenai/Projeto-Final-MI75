570441b06f5db1bd7a7f6a3384906949c72779c7
GUSTAVO DE SOUZA <gustavo_souza12@estudante.sesisenai.org.br>
2025-02-26 21:53:33 -0300

    commit inicio do crud imovel

--- back-end-imobiliaria/Imobiliaria/src/main/java/weg/projetofinal/Imobiliaria/model/entity/Imovel.java
+++ back-end-imobiliaria/Imobiliaria/src/main/java/weg/projetofinal/Imobiliaria/model/entity/Imovel.java
@@ -1,4 +1,20 @@
 package weg.projetofinal.Imobiliaria.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_imovel")
+@Builder
 public class Imovel {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 }
