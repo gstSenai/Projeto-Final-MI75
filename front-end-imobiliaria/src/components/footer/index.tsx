f6013a636902b12e58b997996238b8187dd37037
Caio dos Santos Moura <caio_s_moura@estudante.sesisenai.org.br>
2025-02-28 15:25:48 -0300

    update reajustes

--- front-end-imobiliaria/src/components/footer/index.tsx
+++ front-end-imobiliaria/src/components/footer/index.tsx
@@ -1,4 +1,11 @@
+import { Montserrat } from 'next/font/google';
 import Link from 'next/link';
+
+const montserrat = Montserrat({
+    subsets: ['latin'],
+    weight: ['400', '800'],
+    display: 'swap',
+});
 
 export function Footer() {
     return (
@@ -7,8 +14,8 @@ export function Footer() {
                 <div className="flex justify-between">
                     <section className="pt-24 pl-10 pr-10 flex flex-row gap-32">
                         <div className="text-white flex flex-col gap-3">
-                            <p className="text-xl lg:text-2xl 2xl:text-4xl font-extrabold">Informações</p>
-                            <p className="font-light lg:text-[0.9rem] xl:text-2xl xl:max-w-[585px]">Lorem ipsum dolor sit amet, consectetur adipiscing
+                            <p className="text-xl xl:text-2xl 2xl:text-3xl font-extrabold">Informações</p>
+                            <p className="font-light lg:text-[0.9rem] xl:text-2xl xl:min-w-[400px] xl:max-w-[585px]">Lorem ipsum dolor sit amet, consectetur adipiscing
                                 elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
                             <div className='flex gap-8 pt-8'>
                                 <Link href="https://facebook.com" target="_blank">
@@ -25,31 +32,31 @@ export function Footer() {
                                 </Link>
                             </div>
                         </div>
-                        <div className="text-white lg:flex lg:flex-col lg:gap-3">
-                            <p className="lg:text-2xl text-xl font-extrabold">Serviços</p>
+                        <div className="text-white lg:flex lg:flex-col lg:gap-3 whitespace-nowrap">
+                            <p className="lg:text-2xl text-xl xl:text-3xl font-extrabold">Serviços</p>
                             <div className="font-light lg:flex lg:flex-col lg:gap-3 gap-2 text-[0.9rem]">
-                                <p><a href="#">Avaliações</a></p>
-                                <p><a href="#">Consultoria</a></p>
-                                <p><a href="#">Regularização</a></p>
-                                <p><a href="#">Investimentos</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">Avaliações</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">Consultoria</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">Regularização</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">Investimentos</a></p>
                             </div>
                         </div>
-                        <div className="text-white flex flex-col gap-3">
-                            <p className="lg:text-2xl text-xl font-extrabold">Contato</p>
+                        <div className="text-white flex flex-col gap-3 whitespace-nowrap">
+                            <p className="lg:text-2xl text-xl  xl:text-3xl font-extrabold">Contato</p>
                             <div className="font-light lg:flex lg:flex-col lg:gap-3 gap-2 text-[0.9rem]">
-                                <p><a href="#">Suporte</a></p>
-                                <p><a href="#">Atendimento</a></p>
-                                <p><a href="#">FAQ</a></p>
-                                <p><a href="#">Reclame aqui</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">Suporte</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">Atendimento</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">FAQ</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">Reclame aqui</a></p>
                             </div>
                         </div>
-                        <div className="text-white flex flex-col gap-3 z-10">
-                            <p className="lg:text-2xl text-xl font-extrabold">Links Rápidos</p>
+                        <div className="text-white flex flex-col gap-3 z-10 whitespace-nowrap">
+                            <p className="lg:text-2xl text-xl xl:text-3xl font-extrabold">Links Rápidos</p>
                             <div className="font-light lg:flex lg:flex-col lg:gap-3 gap-2 text-[0.9rem]">
-                                <p><a href="#">Página inicial</a></p>
-                                <p><a href="#">Sobre nós</a></p>
-                                <p><a href="#">Comprar</a></p>
-                                <p><a href="#">Vender</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">Página inicial</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">Sobre nós</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">Comprar</a></p>
+                                <p className='lg:text-xl xl:text-2xl'><a href="#">Vender</a></p>
                             </div>
                         </div>
                     </section>
