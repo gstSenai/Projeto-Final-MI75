96672c9aa0750029b3e5c7a2b277b86174eb71e4
luanaNichelatti <147559793+luanaNichelatti@users.noreply.github.com>
2025-02-28 16:56:08 -0300

    perfil do usuario 50%

--- front-end-imobiliaria/src/app/perfilUsuario/page.tsx
+++ front-end-imobiliaria/src/app/perfilUsuario/page.tsx
@@ -4,24 +4,23 @@ import { FaPhoneAlt, FaEnvelope } from 'react-icons/fa';
 
 export default function PerfilUsuario() {
     return (
-        <div className="flex bg-[#B1ACA2] p-6 rounded -lg shadow-lg gap-3 max-w-2x1 mx-auto w-[65%]">
-            <div className='flex flex-col'>
+        <div className="flex bg-[#B1ACA2] p-6 rounded h-[300px] -lg shadow-lg gap-10 max-w-2x1 mx-auto w-[70%]">
+            <div className='flex flex-col items-center text-center w-[50%]'>
                 <img
                     src="/images/user.jpg"
                     alt="Foto do usuário"
-                    className='w-24 h-32 rounded-full object-cover border-2 border-black ml-10'
+                    className='w-24 h-24 rounded-full object-cover border-2 border-black'
                 />
-                <div className=''>
+                <div className='mt-7 '>
                     <h2 className='text-xl font-bold text-black'>Jéssica Vieira</h2>
                     <p className='text-gray-700'>28 anos</p>
                 </div>
             </div>
 
-            <div className='flex flex-col'>
+            <div className='flex flex-col mr-80'>
                 <h3 className='text-lg font-semibold text-black'>Biografia</h3>
-                <p className='text-gray-700 text-sm'>
-                    Jéssica Vieira está focada em dar um passo importante para o futuro, sonhando em adquirir um terreno ou imóvel,
-                    para construir não apenas um lar, mas um espaço que reflita suas conquistas e ambições.
+                <p className='text-gray-700 text-[15px]'>
+                Jéssica Vieira está focada em dar um passo importante para o futuro, sonhando em adquirir um terreno ou imóvel, para construir não apenas um lar, mas um espaço que reflita suas conquistas e ambições. Ela busca mais do que um simples lugar para morar: quer investir no seu próprio projeto de vida, com segurança e estabilidade para si e para sua família.
                 </p>
 
                 <div className='t-4'>
