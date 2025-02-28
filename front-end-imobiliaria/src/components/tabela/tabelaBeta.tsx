130bd2a0e3f28c5dbfd2f1e15d83eeccc984c2b9
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2025-02-28 15:51:35 -0300

    uptade tabela

--- front-end-imobiliaria/src/components/tabela/tabelaBeta.tsx
+++ front-end-imobiliaria/src/components/tabela/tabelaBeta.tsx
@@ -16,6 +16,10 @@
     ['3567e943', 'Casa do Amizade', 'Locação', 'Permitida', 'Livre'],
     ['4356av01', 'Casa do Bau', 'Locação', 'Permitida', 'Livre'],
     ['3242x322a', 'Casa de Três rios', 'Venda', 'Permitida', 'Vendido'],
+    ['6541e9v564', 'Casa de João pessoa', 'Locação', 'bloqueada', 'Alugado'],
+    ['3567e943', 'Casa do Amizade', 'Locação', 'Permitida', 'Livre'],
+    ['4356av01', 'Casa do Bau', 'Locação', 'Permitida', 'Livre'],
+    ['3242x322a', 'Casa de Três rios', 'Venda', 'Permitida', 'Vendido'],
     ['23we2433', 'Casa de Nereu Ramos', 'Venda', 'bloqueada', 'A venda']
   ];
 
