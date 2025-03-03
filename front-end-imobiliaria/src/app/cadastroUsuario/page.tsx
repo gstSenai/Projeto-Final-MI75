99d1b0d3e6e05dade3d9afa694aa653ca307cd9a
Caio <caio_s_moura@estudante.sesisenai.org.br>
2025-03-03 11:49:37 -0300

    .

---
+++ front-end-imobiliaria/src/app/cadastroUsuario/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header";
+import Tabela from "@/components/tabela";
+
+
+export default function CadastroUsuario() {
+    return (
+        <>
+            <Header/>
+            <Tabela isPropertyTable={true} />
+        </>
+
+    )
+}
