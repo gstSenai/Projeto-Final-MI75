1aad607e756f4de0fadfa82775cfeb75fbc8874c
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2025-03-05 21:37:06 -0300

    PaginaInicial

--- front-end-imobiliaria/src/components/PesquisaPaginaInicial/index.tsx
+++ front-end-imobiliaria/src/components/PesquisaPaginaInicial/index.tsx
@@ -1,17 +1,33 @@
 "use client";
 
+import { Montserrat } from 'next/font/google';
+import { useState } from 'react';
 import Botao from '@/components/Botao';
 
+const montserrat = Montserrat({
+    subsets: ['latin'],
+    weight: ['400', '800'],
+    display: 'swap',
+});
+
 export function PesquisaPaginaInicial() {
+    const [modo, setModo] = useState('comprar');
     return (
-        <div className="bg-[#702632] p-10 rounded-3xl">
-            <div className='flex flex-row text-2xl gap-14 '>
-                <button>Comprar</button>
-                <button>Aluguel</button>
+        <div className={`${montserrat.className} bg-[#702632] pr-6 pl-6 pt-6 rounded-3xl w-[320px]`}>
+            <div className='flex text-lg justify-between'>
+                <button
+                    className={`p-2 px-6 rounded-md ${modo === 'comprar' ? 'bg-[#DFDAD0] font-medium text-black' : 'text-white font-bold'}`}
+                    onClick={() => setModo('comprar')}
+                >Comprar</button>
+                <button
+                    className={`p-2 px-6 rounded-md ${modo === 'alugar' ? 'bg-[#DFDAD0] font-medium text-black' : 'text-white font-bold'}`}
+                    onClick={() => setModo('alugar')}
+                >Aluguel</button>
             </div>
+            <div className='bg-[#702632] p-28 w-0 2xl:w-[800px] rounded-3xl'>
 
-             
 
+            </div>
         </div>
     );
 }
