ff48ed3414f185497bfc37aa6ffa2976427e3b7c
luanaNichelatti <147559793+luanaNichelatti@users.noreply.github.com>
2025-03-07 16:36:01 -0300

    sobre nós 100%

--- front-end-imobiliaria/src/app/sobreNos/page.tsx
+++ front-end-imobiliaria/src/app/sobreNos/page.tsx
@@ -3,30 +3,50 @@ import { Footer } from '@/components/footer';
 
 const montserrat = Montserrat({
     subsets: ['latin'],
-    weight: ['400', '800'],
+    weight: ['400', '800', '900'],
     display: 'swap',
 });
 
 export default function Sobre() {
     return (
-        <div className="bg-[#e3ddd6] min-h-screen flex justify-center p-6 font-montserrat">
-            <div className="max-w-6xl w-full  p-6  ">
+        <div className="bg-[#DFDAD0] min-h-screen flex flex-col items-center p-6 font-montserrat">
+            {/* Seção Sobre Nós */}
+            <div className="relative w-full max-w-6xl mb-12 p-6">
+                <div className="relative rounded-lg overflow-hidden">
+                    <img 
+                        src="/imgSobreNos/capa.png" 
+                        alt="Sobre nós"
+                        className="w-max h-max object-cover "
+                    />
+                    <div className="absolute inset-0 flex items-center p-6">
+                        <div className="text-white max-w-md ml-14 ">
+                            <h2 className="text-[40px] font-black mb-6">Sobre nós</h2>
+                            <p className="text-[15px] w-72">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.        
+                        </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Seção Principal */}
+            <div className=" w-full max-w-6xl p-6  font-montserrat">
                 {/* Missões */}
                 <div className="flex flex-col md:flex-row items-center gap-14 mb-28">
                     <img
-                        src="imgSobreNos/missoes.png"
+                        src="/imgSobreNos/missoes.png"
                         alt="Missão"
                         className="w-full md:w-[50%] rounded-lg shadow-md"
                     />
                     <div>
                         <h2 className="text-[30px] font-bold mb-2">Missões</h2>
                         <p className="text-black">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.              </p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.                        </p>
                     </div>
                 </div>
 
                 {/* Visão */}
-                <div className="flex flex-col md:flex-row-reverse items-center gap-14 mb-8 ">
+                <div className="flex flex-col md:flex-row-reverse items-center gap-14 mb-28">
                     <img
                         src="/imgSobreNos/visao.png"
                         alt="Visão"
@@ -35,32 +55,29 @@ export default function Sobre() {
                     <div>
                         <h2 className="text-[30px] font-bold mb-2">Visão</h2>
                         <p className="text-black">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.              </p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.                        </p>
                     </div>
                 </div>
 
                 {/* Sede */}
-                <div className="flex gap-10 justify-between">
-                    <div className="text-left mt-20 leading-[30px] text-black">
+                <div className="flex flex-col md:flex-row justify-between items-center gap-10">
+                    <div className="text-left leading-[30px] text-black">
                         <h2 className="text-[30px] font-bold mb-2">Sede da HAV Imobiliária</h2>
-                        <p >HAV Imobiliária</p>
-                        <p >Centro, Jaraguá do Sul</p>
-                        <p >CEP 89253-030</p>
-                        <p >SC, Brasil</p>
-                        <p >contato@havimobiliaria.com.br</p>
+                        <p>HAV Imobiliária</p>
+                        <p>Centro, Jaraguá do Sul</p>
+                        <p>CEP 89253-030</p>
+                        <p>SC, Brasil</p>
+                        <p>contato@havimobiliaria.com.br</p>
                     </div>
-                    <div className=" items-center flex">
+                    <div className="flex items-center">
                         <img
-                            src="imgSobreNos/HAV.png"
-                            alt="Missão"
+                            src="/imgSobreNos/HAV.png"
+                            alt="HAV Imobiliária"
                             className="w-48 h-48"
                         />
                     </div>
                 </div>
-
             </div>
-            
         </div>
-        
     );
 }
