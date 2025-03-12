0928860c54c493c1f3850411ced04b844ce79176
luanaNichelatti <147559793+luanaNichelatti@users.noreply.github.com>
2025-03-12 19:28:00 -0300

    90% - pagina ta quebrada

--- front-end-imobiliaria/src/app/paginaCorretores/page.tsx
+++ front-end-imobiliaria/src/app/paginaCorretores/page.tsx
@@ -3,31 +3,11 @@ import { Montserrat } from 'next/font/google';
 
 // Carregando a fonte Inter
 const montserrat = Montserrat({
-    subsets: ['latin'],
-    weight: ['400', '800'],
-    display: 'swap',
+  subsets: ['latin'],
+  weight: ['400', '800'],
+  display: 'swap',
 });
 
-// import Image from "next/image";
-
-// export default function Corretores() {
-//   return (
-//     <div className="p-6 max-w-5xl mx-auto">
-
-//       <h2 className="text-xl font-bold text-gray-800 ">Corretores</h2>
-
-//       <div className="relative w-[1000px] h-[600px] ">
-//         <Image
-//          src="/iconsPaginaCorretores/corretores.png"
-//           alt="Equipe de corretores"
-//           layout="fill"
-//           objectFit="cover"
-//           className="rounded-lg"
-//         />
-//       </div>
-//     </div>
-//   );
-// }
 
 import Image from "next/image";
 
@@ -58,52 +38,70 @@ const equipe = [
     creci: "65432-7",
     telefone: "+55 (49) 99234-5678",
     email: "lucas.oliveira@hav.com.br",
-    imagem:  "/iconsPaginaCorretores/corretor4.png",
+    imagem: "/iconsPaginaCorretores/corretor4.png",
   },
 ];
 
 export default function Equipe() {
-    return (
-      <div className="bg-[#EFE5DA] p-8 rounded-lg shadow-lg  text-center font-montserrat">
-        {/* Título e descrição */}
-        <h2 className="text-2xl font-light uppercase tracking-wider">
-          Equipe <span className="block text-lg font-bold">HAV</span>
-        </h2>
-        <p className="text-gray-700 mt-4 max-w-2xl mx-auto">
-          Na HAV Imobiliária, nossa equipe é mais do que uma força de vendas, somos profissionais dedicados a entender as necessidades de cada cliente, oferecendo um atendimento personalizado e soluções inteligentes para transformar seus projetos em realidade. Juntos, buscamos sempre o melhor para você!
-        </p>
-  
+  return (
+    <div className="bg-[#EFE5DA] p-8 rounded-lg shadow-lg text-center font-montserrat">
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2  mt-16">
-          {equipe.map((corretor, index) => (
-            <div key={index} className="flex flex-col items-center">
+      <div className="p-6 max-w-5xl mx-auto">
 
-              <div className="w-52 h-64 relative rounded-lg overflow-hidden">
-                <Image
-                  src={corretor.imagem}
-                  alt={corretor.nome}
-                  layout="fill"
-                  objectFit="cover"
-                  className="rounded-lg"
-                />
-              </div>
-  
-            <div className='text-left ml-4'>
-                <h3 className="mt-4">{corretor.nome}</h3>
-                <p className="mt-2">CRECI: {corretor.creci}</p>
-                <div className='flex gap-3 mt-2'>
-                <img src="/iconsPaginaCorretores/iconTelefone.png" alt="Telefone" className="w-5 h-5" />
-                    <p className="">{corretor.telefone}</p>
-                </div>
-                <div className='flex gap-4 mt-2 mb-20'>
-                <img src="/iconsPaginaCorretores/iconEmail.png" alt="Telefone" className="w-5 h-5" />
-                    <p className="">{corretor.email}</p>
-                </div>
+      <h2 className="ml-3 text-xl font-bold text-gray-800 text-left mb-10 border-b-2 border-[#702632] pb-2 w-32">
+  Corretores
+</h2>
 
-            </div>
-        </div>
-          ))}
+
+        <div className="relative w-[800px] h-[400px] mb-4 mx-auto">
+          <Image
+            src="/iconsPaginaCorretores/corretores.png"
+            alt="Equipe de corretores"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-lg"
+          />
         </div>
       </div>
-    );
-  }
+      <div className='border-b border-[#000000] pb-2 w-80 mx-auto opacity-30 mb-8'></div>
+      <h2 className="text-2xl font-light uppercase tracking-wider">
+        Equipe <span className="block text-lg font-bold">HAV</span>
+      </h2>
+      <p className="text-gray-700 mt-4 max-w-2xl mx-auto">
+        Na HAV Imobiliária, nossa equipe é mais do que uma força de vendas, somos profissionais dedicados a entender as necessidades de cada cliente, oferecendo um atendimento personalizado e soluções inteligentes para transformar seus projetos em realidade. Juntos, buscamos sempre o melhor para você!
+      </p>
+
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2  mt-16">
+        {equipe.map((corretor, index) => (
+          <div key={index} className="flex flex-col items-center">
+
+            <div className="w-52 h-64 relative rounded-lg overflow-hidden">
+              <Image
+                src={corretor.imagem}
+                alt={corretor.nome}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-lg"
+              />
+            </div>
+
+            <div className='text-left ml-4'>
+              <h3 className="mt-4">{corretor.nome}</h3>
+              <p className="mt-2">CRECI: {corretor.creci}</p>
+              <div className='flex gap-3 mt-2'>
+                <img src="/iconsPaginaCorretores/iconTelefone.png" alt="Telefone" className="w-5 h-5" />
+                <p className="">{corretor.telefone}</p>
+              </div>
+              <div className='flex gap-4 mt-2 mb-20'>
+                <img src="/iconsPaginaCorretores/iconEmail.png" alt="Telefone" className="w-5 h-5" />
+                <p className="">{corretor.email}</p>
+              </div>
+
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
