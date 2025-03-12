febfa68266ea0f0e477d26447fb43e3a324cdd00
KauanMartim <kauanmartimoares@gmail.com>
2025-03-12 16:58:43 -0300

    Pagina de login alterada

--- front-end-imobiliaria/src/components/AuthForm/index.tsx
+++ front-end-imobiliaria/src/components/AuthForm/index.tsx
@@ -16,13 +16,13 @@ interface AuthFormProps {
 const AuthForm: React.FC<AuthFormProps> = ({ title, buttonText }) => {
   return (
     <div className="font-montserrat flex h-screen items-center justify-center bg-gray-100">
-      <div className="bg-white shadow-md rounded-lg flex flex-col md:flex-row w-4/5 max-w-5xl bg-[#EBE8DE]">
+      <div className="shadow-md rounded-lg flex flex-col md:flex-row w-4/5 max-w-5xl bg-[#EBE8DE]">
         {/* Lado Esquerdo */}
-        <div className="w-full md:w-[35%] flex flex-col py-24 items-center justify-center bg-gradient-to-b from-[rgba(223,218,208,1)] to-[rgba(115,115,115,0.3)]">
+        <div className="w-full md:w-[40%] flex flex-col py-20 items-center justify-center bg-gradient-to-b from-[rgba(223,218,208,1)] to-[rgba(115,115,115,0.3)] rounded-l-lg">
           <h1 className="text-[34px] font-bold text-[#280202] tracking-[2px]">BEM VINDO</h1>
           <h2 className="text-3xl font-semibold text-[#280202] tracking-[6px]">HAV</h2>
           <img src="/logos/logoLogin.png" alt="Logo" className="w-32 my-4" />
-          <button className="bg-white border border-gray-300 text-[#702632] text-[12px] font-bold py-2 px-4 rounded-lg flex items-center mt-4">
+          <button className="bg-white border border-gray-300 text-[#702632] text-[14px] font-bold py-2.5 px-2 w-[240px] rounded-xl flex justify-center items-center mt-4">
             <img src="/loginIcons/google-icon.png" alt="Google" className="w-5 h-5 mr-2 " />
             Continue com o Google
           </button>
@@ -31,7 +31,7 @@ const AuthForm: React.FC<AuthFormProps> = ({ title, buttonText }) => {
 
         {/* Lado Direito */}
         <div className="w-full md:w-[65%] flex flex-col justify-center items-center p-6">
-          <h2 className="text-4xl font-bold text-[#702632] tracking-[6px] mb-4">{title}</h2>
+          <h2 className="text-4xl font-bold text-[#702632] tracking-[6px] mb-8">{title}</h2>
           <form>
             <div className="mb-4">
               <label className=" text-gray-700 ">Email</label>
@@ -56,12 +56,13 @@ const AuthForm: React.FC<AuthFormProps> = ({ title, buttonText }) => {
                 <input type="checkbox" className="mr-2" />
                 Lembrar-me
               </label>
-              <a href="#" className="text-blue-500">Esqueceu a senha?</a>
+              <a href="#" className="text-black underline hover:text-[#702632]">Esqueceu a senha?</a>
             </div>
-
-            <button className="w-[45%] font-bold bg-[#FFFBFB] text-[#702632] py-2 rounded-lg">
-              {buttonText}
-            </button>
+            <div className="flex justify-center mt-8">
+              <button className="md: w-[45%] font-bold bg-[#FFFBFB] text-[#702632] py-2 rounded-lg">
+                {buttonText}
+              </button>
+            </div>
 
           </form>
         </div>
