9fa49d8f71040f2399c9c028f194ea5290f9146e
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2025-03-19 19:07:05 -0300

    Update page.tsx

--- front-end-imobiliaria/src/app/paginaAgendamento/page.tsx
+++ front-end-imobiliaria/src/app/paginaAgendamento/page.tsx
@@ -11,6 +11,10 @@ export default function paginaAgendamento() {
         <div className='px-4 sm:px-8 md:px-10 lg:px-14 xl:px-20'>
         <section>
 
+        <h2 className="z-0 text-[#702632] absolute w-auto lg:w-[650px] text-center font-bold text-xl lg:text-3xl xl:text-5xl 2xl:text-[64px]">Escolha a data, o horário e o corretor para agendar sua visita!</h2>
+                    <Image className="relative opacity-[7%] -z-10" src="/formularioContato/logoGrande.png" alt="Gerenciamento de Imóveis" width={911} height={833} />
+
+
 
 
         </section>
