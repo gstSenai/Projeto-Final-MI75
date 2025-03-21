7b23005d72d91592d42a3002ffeb44d54461e06b
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2025-03-21 18:21:24 -0300

    feito

--- front-end-imobiliaria/src/components/header/index.tsx
+++ front-end-imobiliaria/src/components/header/index.tsx
@@ -1,4 +1,7 @@
+"use client";
 import { Inter } from 'next/font/google';
+import { useState } from 'react';
+import Image from 'next/image';
 
 // Carregando a fonte Inter
 const inter = Inter({
@@ -8,6 +11,7 @@ const inter = Inter({
 });
 
 export function Header() {
+    const [mostrarAvançado, setMostrarAvançado] = useState(false);
     return (
         <>
             <header className="font-inter bg-[#DFDAD0] px-10 pt-6 max-lg:px-10 lg:px-30 xl:px-20 font-medium">
@@ -44,10 +48,47 @@ export function Header() {
                         <div className="flex flex-row items-center max-lg:hidden">
                             <img src="/imagensHeader/PERFIL SEM LOGIN.png" alt="Perfil sem login" className="w-12 lg:w-[50px]" />
                         </div>
-                        <div className="flex flex-row items-center lg:hidden">
-                            <img src="/imagensHeader/HAMBURGUER.png" alt="Perfil sem login" className="w-12 lg:w-[50px]" />
-                        </div>
+                        <button onClick={() => setMostrarAvançado(!mostrarAvançado)}>
+                            <div className="flex flex-row items-center lg:hidden">
+                                <img src="/imagensHeader/HAMBURGUER.png" alt="Perfil sem login" className="w-12 lg:w-[50px]" />
+                            </div>
+                        </button>
                     </div>
+
+
+                    {mostrarAvançado && (
+                        <>
+                            <div className='bg-[#DFDAD0] absolute z-10'>
+                                <nav>
+                                    <ul className="flex flex-col text-xl whitespace-nowrap gap-12 lg:gap-6 text-[#303030]">
+                                        <div className='flex p-[0.5px]'></div>
+                                        <li className='flex items-center gap-2 text-2xl'>
+                                            <Image src="/iconsDentrodoHeader/logoMinuscula.png" alt="logo Minuscula" width={24} height={24} />
+                                            <a href="/PaginaInicial">Início</a>
+                                        </li>
+                                        <li className='flex items-center gap-2 text-2xl'>
+                                            <Image src="/iconsDentrodoHeader/casa.png" alt="logo Minuscula" width={24} height={24} />
+                                            <a href="#">Propriedades</a>
+                                        </li>
+                                        <li className='flex items-center gap-2 text-2xl'>
+                                            <Image src="/iconsDentrodoHeader/corretores.png" alt="logo Minuscula" width={24} height={24} />
+                                            <a href="#">Corretores</a>
+                                        </li>
+                                        <li className='flex items-center gap-2 text-2xl'>
+                                            <Image src="/iconsDentrodoHeader/informacao.png" alt="logo Minuscula" width={24} height={24} />
+                                            <a href="/sobreNos">Sobre nós</a>
+                                        </li>
+                                    </ul>
+                                </nav>
+
+
+
+                            </div>
+                        </>
+                    )}
+
+
+
                 </section >
             </header>
         </>
