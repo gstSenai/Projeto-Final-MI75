de74f0e887c7e3bc99a8d41fec1e15acc7c3cdc0
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2025-04-11 21:48:14 -0300

    ajustes

--- front-end-imobiliaria/src/components/NumeroAtualizaveis/ImoveisDisponiveis.tsx
+++ front-end-imobiliaria/src/components/NumeroAtualizaveis/ImoveisDisponiveis.tsx
@@ -1,9 +1,33 @@
+"use client";
+import { useEffect, useState, useCallback } from "react"
+import request from "@/routes/request"
 
-export function ImoveisDisponiveis(){
-return(
-    <div className="font-semibold tracking-[7%] text-3xl">
-        <p>00000</p>
-    </div>
+
+export function ImoveisDisponiveis() {
+    const [isLoading, setIsLoading] = useState(false)
+
+
+    const getImoveisDisponiveis = useCallback(async () => {
+        setIsLoading(true);
+        try {
+            const response = await request("GET", `http://localhost:9090/imovel/getAll`);
+
+        } catch (error) {
+            console.error("Erro ao buscar barra de status:", error);
+        } finally {
+            setIsLoading(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        getImoveisDisponiveis();
+    }, [getImoveisDisponiveis]);
+
+
+    return (
+        <div className="font-semibold tracking-[7%] text-3xl">
+            <p>00000</p>
+        </div>
 
 
     );
