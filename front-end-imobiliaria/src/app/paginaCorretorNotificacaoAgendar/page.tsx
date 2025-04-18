8502ec7ba394276d1db9b7aff376b40cd58192d5
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2025-04-17 23:54:07 -0300

    começos

---
+++ front-end-imobiliaria/src/app/paginaCorretorNotificacaoAgendar/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { Header } from '@/components/header';
+import { Footer } from '@/components/footer';
+import { LoadingWrapper } from '@/components/loading/loadingServer';
+import { PaginaHistoricoCorretorChamar } from '@/components/paginaHistorico/PaginaHistoricoCorretorChamar';
+import Image from 'next/image';
+
+
+
+export function paginaCorretorNotificacaoAgendar() {
+    return (
+        <>
+            <LoadingWrapper>
+                <header>
+                    <Header />
+                </header>
+                <div className='pt-10 px-6 max-lg:px-6 lg:px-20 xl:px-16'>
+                    <section>
+                        <div>
+                            <h1 className="font-bold text-lg md:text-xl lg:text-2xl">Notificações Agendamentos</h1>
+                            <div className="border-t-2 border-[#702632] w-[130px] mt-1"></div>
+                        </div>
+                        <div className='px-[75px] pt-8 pb-44'>
+                         
+                        </div>
+                    </section>
+                </div>
+                <footer>
+                    <Footer />
+                </footer>
+            </LoadingWrapper>
+        </>
+    );
+}
