c07dc17faaeb5d50023783bf670801bf589b4618
Kauã Dal Santo <kaua_santo@estudante.sesisenai.org.br>
2025-04-22 11:23:24 -0300

    ajustes

--- front-end-imobiliaria/src/app/paginaEditor/page.tsx
+++ front-end-imobiliaria/src/app/paginaEditor/page.tsx
@@ -15,7 +15,7 @@ import { Header } from '@/components/header';
 import { useRouter } from 'next/navigation';
 import RotaPrivada from '@/components/RotaPrivada';
 
-export default function PaginaAdministrador() {
+export default function paginaEditor() {
     const router = useRouter();
     return (
         <LoadingWrapper>
@@ -24,15 +24,15 @@ export default function PaginaAdministrador() {
 
                 <div className={`${montserrat.className} pt-3 pb-24 lg:pt-14 lg:pb-52`}>
                     <main className="relative flex h-[500px] xl:h-[450px]  rounded-[20px] mx-auto overflow-hidden pb-24 lg:pt-14 lg:pb-52 w-11/12 shadow-lg transition-all duration-300 hover:shadow-xl">
-                        <Image src="/imagensPaginaEditor-Adm/montanhaPaginaAdministrador.png" alt="cidade da Pagina do Editor" layout="fill" objectFit="cover" quality={100} className="transition-transform duration-700 hover:scale-105" />
+                        <Image src="/imagensPaginaEditor-Adm/cidadePaginaEditor.png" alt="cidade da Pagina do Editor" layout="fill" objectFit="cover" quality={100} className="transition-transform duration-700 hover:scale-105" />
 
                         <div className="absolute inset-0 flex flex-col justify-center items-center 2xl:items-start text-white p-8 md:pl-16 2xl:pl-32 bg-gradient-to-r from-black/20 to-transparent">
-                            <h1 className="text-xl lg:text-2xl text-center 2xl:text-start font-bold mb-4">Bem-vindo à Área do Administrador!</h1>
+                            <h1 className="text-xl lg:text-2xl text-center 2xl:text-start font-bold mb-4">Bem- vindo à Área do Editor!</h1>
 
                             <div className="border-t-4 border-vermelho w-[265px] md:w-[405px] 2xl:w-[405px] my-6 transform transition-all duration-300 hover:w-[300px] md:hover:w-[450px]"></div>
 
                             <p className="text-lg lg:text-xl font-normal text-center 2xl:text-start max-w-4xl leading-relaxed">
-                                Aqui você encontra todas as ferramentas necessárias para gerenciar todos os imóveis, usuários, cadastro de imóveis e Relatórios e Análises de dados.
+                                Aqui você encontra todas as ferramentas necessárias para gerenciar todos os imóveis junto de ferramentas de cadastro de imóveis
                             </p>
                         </div>
                     </main>
@@ -40,13 +40,14 @@ export default function PaginaAdministrador() {
                     <section className="mt-16">
                         <h2 className='flex justify-center text-center text-2xl lg:text-3xl font-medium py-10 text-gray-800'>Explore nossos recursos:</h2>
 
-                        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 md:px-8'>
+                        <div className='flex flex-col sm:flex-row justify-center items-center gap-8 px-4 md:px-8'>
                             <div className='flex flex-col items-center p-6 rounded-xl'>
                                 <div className="p-4 rounded-full mb-4">
                                     <Image src="/imagensPaginaEditor-Adm/gerenciamentoImoveis.png" alt="Gerenciamento de Imóveis" width={75} height={75} className="transition-transform duration-300 hover:scale-110" />
                                 </div>
 
-                                <p className='text-lg lg:text-xl font-medium text-gray-700 text-center leading-tight mb-6 lg:whitespace-nowrap'>Cadastro Imóveis</p>
+                                <p className='text-lg lg:text-xl font-medium text-gray-700 text-center leading-tight mb-6 lg:whitespace-nowrap'>Gerenciamento de
+                                    Imovéis</p>
 
                                 <Botao className='w-full bg-vermelho h-10 hover:bg-vermelho/90 transition-colors duration-300' onClick={() => router.push("/cadastroImovel")} texto="Gerenciar" />
                             </div>
@@ -56,9 +57,10 @@ export default function PaginaAdministrador() {
                                     <Image src="/imagensPaginaEditor-Adm/gerenciamentoProprietarios.png" alt="Cadastro de Proprietários" width={75} height={75} className="transition-transform duration-300 hover:scale-110" />
                                 </div>
 
-                                <p className='text-lg lg:text-xl font-medium text-gray-700 text-center leading-tight mb-6'>Cadastro Proprietários</p>
+                                <p className='text-lg lg:text-xl font-medium text-gray-700 text-center leading-tight mb-6'>Gerenciamento de
+                                    proprietários</p>
 
-                                <Botao className='w-full bg-vermelho h-10 hover:bg-vermelho/90 transition-colors duration-300' onClick={() => router.push("/cadastroProprietario")} texto="Gerenciar" />
+                                <Botao className=' bg-vermelho h-10 hover:bg-vermelho/90 transition-colors duration-300' onClick={() => router.push("/cadastroProprietario")} texto="Gerenciar" />
                             </div>
 
                         </div>
